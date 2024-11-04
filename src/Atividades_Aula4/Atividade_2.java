61be245414aec5c7e973be5d87622a5766b6cba6
rodrigo oliveira <pracimaestudos@gmail.com>
2024-11-04 16:50:04 -0300

    att

--- src/Atividades_Aula4/Atividade_2.java
+++ src/Atividades_Aula4/Atividade_2.java
@@ -10,6 +10,40 @@ public class Atividade_2 {
         
         
         
+        
+       
+        int[] vetor = new int[10];
+        int soma = 0;
+
+        // Leitura dos 10 números inteiros e armazenamento no vetor
+        System.out.println("Digite 10 números inteiros:");
+        for (int i = 0; i < 10; i++) {
+            vetor[i] = leia.nextInt();
+            soma += vetor[i]; // Soma de todos os elementos
+        }
+
+        
+        System.out.println("\nElementos nos índices ímpares:");
+        for (int i = 1; i < 10; i += 2) {
+            System.out.print(vetor[i] + " ");
+        }
+
+        // Exibindo elementos pares
+        System.out.println("\n\nElementos pares:");
+        for (int i = 0; i < 10; i++) {
+            if (vetor[i] % 2 == 0) {
+                System.out.print(vetor[i] + " ");
+            }
+        }
+
+        // Calculando a média
+        double media = (double) soma / 10;
+
+        
+        System.out.println("\n\nSoma: " + soma);
+        System.out.printf("Média: %.2f\n", media);
+
+       
 
         
         
